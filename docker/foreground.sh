16ba2c1b81b4647c454fa76cfb915cbf2da8a240
Stephan Maximilian Huber <stephan@factorial.io>
2016-04-01 10:29:11 +0200

    Fix a regression when starting the container again.

--- docker/foreground.sh
+++ docker/foreground.sh
@@ -6,4 +6,9 @@ trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 # sed -i "s_/var/www/html_${WEB_ROOT}_g" /etc/apache2/sites-available/000-default.conf
 
 source /etc/apache2/envvars
+
+# Delete any leftovers
+rm -rf $APACHE_PID_FILE
+
+# Start apache
 apache2 -D FOREGROUND
